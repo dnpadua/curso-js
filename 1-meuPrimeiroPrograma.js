835f14558cdc63b7b567041679e3a541892cf718
dnpadua <37667481+dnpadua@users.noreply.github.com>
2020-10-06 03:19:37 -0300

    Trabalhando com variaveis

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,9 +1,12 @@
-console.log("Meu primeiro programa: trabalhando com variáveis");
+console.log("Meu Primeiro programa: trabalhando com variáveis");
 
 const idade = 26;
 
+console.log("idade");
 console.log(idade);
 console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
 
 const idadeSomada = idade+2;
 console.log(idadeSomada);
