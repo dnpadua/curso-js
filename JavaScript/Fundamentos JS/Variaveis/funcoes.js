61026fae3feb9a5771e1dbab1986b413e6508a50
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-09-16 22:16:09 -0300

    Update funcoes.js

--- JavaScript/Fundamentos JS/Variaveis/funcoes.js
+++ JavaScript/Fundamentos JS/Variaveis/funcoes.js
@@ -2,4 +2,19 @@ function imprimeTexto(texto) {
     console.log(texto)
 }
 
-imprimeTexto("Olá mundo!");
+imprimeTexto('Bem vindo(a)!');
+
+let listaMarca = ['Ford', 'Chevrolet', 'Fiat']
+let listaModelo = ['Ka', 'Onix', 'Argo']
+
+const exibeLista = (lista, descricao) => {
+    e = ''
+        for (let i = 0; i < lista.length; i++){
+            e += '\n'+ descricao + lista [i]
+    }
+    return e
+}
+
+console.log('Nossa loja trabalha com os seguintes carros:')
+console.log(exibeLista(listaMarca, 'Marca: '))
+console.log(exibeLista(listaModelo, "Modelo: "))
