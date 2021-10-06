0a89632168326f6b3615e1b27f489c2659e58bc5
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-10-05 21:36:57 -0300

    Create print-infos.js

---
+++ JavaScript/JavaScript para back-end/Fundamentos JS Objetos/print-infos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "Daniel",
+    idade: 23,
+    cpf: "123.456.789-00",
+    email: "danielpadua@email.com"
+}
+
+console.log(cliente.nome);
+
+console.log(`Tenho ${cliente.idade} anos, meu e-mail é ${cliente.email}.`);
