2b4ff7345d6ea63e3071e76598e61a671b47b89c
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-10-06 06:56:12 -0300

    Create acessar-fones.js

---
+++ JavaScript/JavaScript para back-end/Fundamentos JS Objetos/acessar-fones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Daniel",
+    idade: 23,
+    cpf: "123.456.789-00",
+    email: "danielpadua@email.com",
+    fones: ["5563981177788", "55988775566"]
+}
+
+cliente.fones.forEach(fone => console.log(fone));
