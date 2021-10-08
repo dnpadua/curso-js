8b01b871f4e77a0ea4c01560b59814467dc90391
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-10-08 00:34:24 -0300

    Update compondo-objetos.js

---
+++ JavaScript/JavaScript para back-end/Fundamentos JS Objetos/compondo-objetos.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: "Daniel",
+    idade: 23,
+    cpf: "123.456.789-00",
+    email: "danielpadua@email.com",
+    fones: ["5563981177788", "55988775566"]
+}
+
+cliente.dependentes = {
+    nome: "Percivania",
+    parentesco: "Mâe",
+    dataNasc: "1968"
+}
+
+console.log(cliente);
+
+cliente.dependentes.nome = "Percivania Silva";
+
+console.log(cliente)
